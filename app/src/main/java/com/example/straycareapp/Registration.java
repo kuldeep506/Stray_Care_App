8badf420aa46830b223995f9c32188dfb58ee953
Kuldeep Singh <hard31259@gmail.com>
2022-05-09 11:43:43 +0530

    assigned variables to fields and setting adapters to Spinners

--- app/src/main/java/com/example/straycareapp/Registration.java
+++ app/src/main/java/com/example/straycareapp/Registration.java
@@ -3,17 +3,34 @@ package com.example.straycareapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class Registration extends AppCompatActivity {
+
+    String[] genderArr={"Male","Female","Unknown"};
+    String[] conditionArr={"Normal","Mild","Severe"};
+
+    /***/
     FirebaseFirestore db;
     CollectionReference collectionReference;
     FirebaseStorage storage;
@@ -33,10 +50,44 @@ public class Registration extends AppCompatActivity {
         imageStorageReference = storage.getReference();
         auth = FirebaseAuth.getInstance();
 
+        /**Buttons*/
         Button captureImage =findViewById(R.id.imageCaptureBtn);
         Button sendOTP =findViewById(R.id.sendOTPBtn);
         Button validateOTP =findViewById(R.id.validateOTPBtn);
         Button finalSubmit =findViewById(R.id.SubmitBtn);
+
+        /** EditTexts*/
+        AutoCompleteTextView animalType=findViewById(R.id.animalType);
+        EditText description=findViewById(R.id.descriptionText);
+        EditText address=findViewById(R.id.address);
+        EditText senderName=findViewById(R.id.senderName);
+        EditText phoneNumber=findViewById(R.id.phoneNumber);
+        EditText enterOTPBox=findViewById(R.id.enterOTPBox);
+
+        /** Imageview*/
+        ImageView imageView=findViewById(R.id.AnimalImage);
+
+        /**Spinners*/
+        Spinner gender=findViewById(R.id.genderSpinner);
+        Spinner condition=findViewById(R.id.conditionSpinner);
+
+        /** Initiating and Setting adapters to Spinners*/
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, genderArr);
+        gender.setAdapter(genderAdapter);
+
+        ArrayAdapter<String> conditionAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, conditionArr);
+        condition.setAdapter(conditionAdapter);
+
+
+        /** sending OTP using Phone number*/
+        sendOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
