bfe561748ef8b9061705acc79009a75779f4f60c
Kuldeep Singh <hard31259@gmail.com>
2022-05-11 00:21:40 +0530

    OTP service Integration completed using google firebase

--- app/src/main/java/com/example/straycareapp/Registration.java
+++ app/src/main/java/com/example/straycareapp/Registration.java
@@ -1,12 +1,11 @@
 package com.example.straycareapp;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -21,11 +20,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
@@ -84,6 +79,7 @@ public class Registration extends AppCompatActivity {
         AutoCompleteTextView animalType = findViewById(R.id.animalType);
         EditText description = findViewById(R.id.descriptionText);
         EditText address = findViewById(R.id.address);
+        EditText city = findViewById(R.id.city);
         EditText senderName = findViewById(R.id.senderName);
         EditText phoneNumber = findViewById(R.id.phoneNumber);
         enterOTPBox = findViewById(R.id.enterOTPBox);
@@ -126,7 +122,7 @@ public class Registration extends AppCompatActivity {
 
                                 @Override
                                 public void onCodeSent(@NonNull @NotNull String s, @NonNull @NotNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                                    OTPByFirebase=s;
+                                    OTPByFirebase = s;
                                     super.onCodeSent(s, forceResendingToken);
                                     Toast.makeText(getApplicationContext(), "OTP Sent", Toast.LENGTH_LONG).show();
                                 }
@@ -140,58 +136,50 @@ public class Registration extends AppCompatActivity {
         validateOTP.setOnClickListener(v -> {
             PhoneAuthCredential credential = PhoneAuthProvider.getCredential(OTPByFirebase, enterOTPBox.getText().toString());
 
-            FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
-                    Toast.makeText(getApplicationContext(), "Sign In", Toast.LENGTH_LONG).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull @NotNull Exception e) {
-                    Toast.makeText(getApplicationContext(), "Sign In Fail", Toast.LENGTH_LONG).show();
-                }
-            });
+            FirebaseAuth.getInstance().signInWithCredential(credential)
+
+                    .addOnCompleteListener(task -> Toast.makeText(getApplicationContext(), "Sign In", Toast.LENGTH_LONG).show())
+
+                    .addOnFailureListener(e -> Toast.makeText(getApplicationContext(), "Sign In Fail", Toast.LENGTH_LONG).show());
         });
 
         /** Button to Capture Image*/
         captureImage.setOnClickListener(v -> {
+                if (TextUtils.isEmpty(animalType.getText().toString().trim())) {
+                    animalType.setError("Field can't be Empty");
+                } else if (TextUtils.isEmpty(description.getText().toString().trim())) {
+                    description.setError("Field can't be Empty");
+                } else if (TextUtils.isEmpty(address.getText().toString().trim())) {
+                    address.setError("Field can't be Empty");
+//                } else if (TextUtils.isEmpty(senderName.getText().toString().trim())) {
+//                    senderName.setError("Field can't be Empty");
+                }else if (TextUtils.isEmpty(city.getText().toString().trim())) {
+                    city.setError("Field can't be Empty");
+                }
+                else {
+                String fileName = "animalPhoto";
+                File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                try {
+                    File imageFile = File.createTempFile(fileName, ".jpg", storageDirectory);
 
-//            if (name.getText().toString().isEmpty() || course.getText().toString().isEmpty() ||
-//                    phone.getText().toString().isEmpty() || phone.getText().toString().isEmpty()) {
-//                String stName = name.getText().toString().trim();
-//                String phNO = phone.getText().toString().trim();
-//                String StStop = stop.getText().toString().trim();
-//                String stCourse = course.getText().toString().trim();
-//                String RegistrationDate = date.getText().toString().trim();
-//                Toast.makeText(getApplicationContext(), "Fill Above Details", Toast.LENGTH_LONG).show();
-//                if (TextUtils.isEmpty(stName)) {
-//                    name.setError("Name can't be Empty");
-//                } else if (TextUtils.isEmpty(phNO)) {
-//                    phone.setError("Phone can't be Empty");
-//                } else if (TextUtils.isEmpty(StStop)) {
-//                    stop.setError("Stop can't be Empty");
-//                } else if (TextUtils.isEmpty(stCourse)) {
-//                    course.setError("course can't be Empty");
-//                } else if (TextUtils.isEmpty(RegistrationDate)) {
-//                    date.setError("Date can't be Empty");
-//                }
-//            } else {
-//                save.setVisibility(View.VISIBLE);
-            String fileName = "animalPhoto";
-            File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-            try {
-                File imageFile = File.createTempFile(fileName, ".jpg", storageDirectory);
+                    currentPhotoPath = imageFile.getAbsolutePath();
 
-                currentPhotoPath = imageFile.getAbsolutePath();
+                    imageUri = FileProvider.getUriForFile(this, "com.example.straycareapp.fileprovider", imageFile);
 
-                imageUri = FileProvider.getUriForFile(this, "com.example.straycareapp.fileprovider", imageFile);
-
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, 1);
-            } catch (IOException e) {
-                e.printStackTrace();
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+        });
+
+        /**
+         * Button to send data to the Database
+         */
+        finalSubmit.setOnClickListener(v -> {
+
         });
     }
 
@@ -201,7 +189,7 @@ public class Registration extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1 && resultCode == RESULT_OK) {
             animalImage.setImageURI(imageUri);
-            Bitmap bitmapImage = BitmapFactory.decodeFile(currentPhotoPath);
+//            Bitmap bitmapImage = BitmapFactory.decodeFile(currentPhotoPath);
 //            imageUri = Util.compress(this, bitmapImage);// reducing the size of image
 
         }
