cdc1a9f0a10e4816859cd6c1f25ce8c2c729a53f
Kuldeep Singh <hard31259@gmail.com>
2022-05-22 13:46:41 +0530

    Completed RequestList.java internal implementation

--- app/src/main/java/com/example/straycareapp/FullDetails.java
+++ app/src/main/java/com/example/straycareapp/FullDetails.java
@@ -10,5 +10,6 @@ public class FullDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_details);
+
     }
 }
