8e1f8a680670f5f02bad476f1459b6202dcf66d2
Kuldeep Singh <hard31259@gmail.com>
2022-05-22 13:31:00 +0530

    implemented RequestListAdapter

---
+++ app/src/main/java/com/example/straycareapp/FullDetails.java
@@ -0,0 +1,14 @@
+package com.example.straycareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FullDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_details);
+    }
+}
